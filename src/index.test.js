e146d818471846931c8f92e28d3788856e13f232
zhanghong <hong.zhang@axinan.com>
2023-07-26 15:20:48 +0800

    feat: add jest

--- src/index.test.js
+++ src/index.test.js
@@ -1,18 +1,38 @@
 /*
  * @Author: Hong.Zhang
  * @Date: 2023-07-24 17:03:16
- * @Description: 
+ * @Description:
  */
-var objectXss = require('./index');
+var objectXss = require("./index");
 
-console.log(objectXss(undefined));
-console.log(objectXss(null));
-console.log(objectXss(`<html>test</html>`));
-console.log(objectXss({
-  html: `<html>html</html>`,
-  obj: {
-    head: `<head>head</head>`,
-    body: `<body>body</body>`,
-  },
-  arr: ['<script>script</script>']
-}));
+test("process undefined", () => {
+  expect(objectXss(undefined)).toBeUndefined();
+});
+
+test("process null", () => {
+  expect(objectXss(null)).toBeNull();
+});
+
+test("process string", () => {
+  expect(objectXss("<html>test</html>")).toBe("&lt;html&gt;test&lt;/html&gt;");
+});
+
+test("process object", () => {
+  expect(
+    objectXss({
+      html: `<html>html</html>`,
+      obj: {
+        head: `<head>head</head>`,
+        body: `<body>body</body>`,
+      },
+      arr: ["<script>script</script>"],
+    })
+  ).toStrictEqual({
+    html: "&lt;html&gt;html&lt;/html&gt;",
+    obj: {
+      head: "&lt;head&gt;head&lt;/head&gt;",
+      body: "&lt;body&gt;body&lt;/body&gt;",
+    },
+    arr: ["&lt;script&gt;script&lt;/script&gt;"],
+  });
+});
